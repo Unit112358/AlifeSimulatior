1f029b5b6e95205d4efc5788e8c9398f6f8dccf4
Hiro33 <chocobo1355@gmail.com>
2019-06-15 18:54:22 +0900

    change make.bat

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
+@echo off
 cd /d %~dp0
 
 set gpparg=-mwindows -DDX_GCC_COMPILE -DDX_NON_INLINE_ASM -LDxLib -lDxLib -lDxUseCLib -lDxDrawFunc -ljpeg -lpng -lzlib -ltiff -ltheora_static -lvorbis_static -lvorbisfile_static -logg_static -lbulletdynamics -lbulletcollision -lbulletmath -lopusfile -lopus -lsilk_common -lcelt
