b8053a2dbc991416d1491f0313b6a24c5dc5cef9
Hiro33 <chocobo1355@gmail.com>
2019-06-15 19:17:02 +0900

    使いやすくした

--- make.bat
+++ make.bat
@@ -2,8 +2,23 @@
 cd /d %~dp0
 
 set gpparg=-mwindows -DDX_GCC_COMPILE -DDX_NON_INLINE_ASM -LDxLib -lDxLib -lDxUseCLib -lDxDrawFunc -ljpeg -lpng -lzlib -ltiff -ltheora_static -lvorbis_static -lvorbisfile_static -logg_static -lbulletdynamics -lbulletcollision -lbulletmath -lopusfile -lopus -lsilk_common -lcelt
+
+if "%1" == "" goto MAIN
+
+set filename=%1
+goto GPP
+
+:MAIN
 set filename=main
+
+:GPP
+if NOT EXIST %filename%.cpp goto NOTFOUNDFILE
 
 g++ -o %filename%.exe %filename%.cpp %gpparg%
 %filename%.exe
 del %filename%.exe
+exit /B
+
+:NOTFOUNDFILE
+echo not found %filename%
+exit /B
